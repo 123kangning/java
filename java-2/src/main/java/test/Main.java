74729a67a7d76812e116e382eb88df40aa3c28d6
123kangning <3390142252@qq.com>
2022-09-06 20:11:55 +0800

    1

--- java-2/src/main/java/test/Main.java
+++ java-2/src/main/java/test/Main.java
@@ -1,13 +1,9 @@
 package test;
 
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int x=30;
         int[] numbers=new int[x];
-        x=60;
         System.out.println(numbers.length);
     }
-
 }
