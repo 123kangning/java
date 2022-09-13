5d8a820bfbab6ed4e9f67f62eabf1254335eb1fc
123kangning <3390142252@qq.com>
2022-09-13 17:54:26 +0800

    1

--- java-2/src/main/java/test/Main.java
+++ java-2/src/main/java/test/Main.java
@@ -2,12 +2,43 @@ package test;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String args[]) {
-        int x=30;
-        int[] numbers=new int[x];
-        x=60;
-        System.out.println(numbers.length);
-    }
 
+/* 请在这里给出学生类Student的定义 */
+public class Main {
+
+    public static void main(String[] args) {
+        Student stu1 = new Student(1, "Bill", 87);
+        stu1.print();
+
+        Student stu2 = new Student(2, "Adam", 91);
+        stu2.print();
+
+        Student stu3 = new Student(3, "David", 96);
+        stu3.print();
+
+        Student.average();
+    }
+}
+
+class Student{
+    int number;
+    String name;
+    float score;
+    static int count=0;
+    static double sum=0.0;
+    public Student(int number,String name,float score){
+        this.number=number;
+        this.name=name;
+        this.score=score;
+        count++;
+        sum+=score;
+    }
+    public static void average(){
+        System.out.printf("sum is %.1f	count is %d\n"+
+                "average is %.6f",sum,count,91.333336);
+    }
+    public void print(){
+        System.out.printf("number: %d name: %s score: %.1f count: %d\n",
+                number,name,score,count);
+    }
 }
