6a2c55356ecf4be74c9be429b62bc2a48e77e3fd
123kangning <3390142252@qq.com>
2022-09-16 16:33:58 +0800

    1

--- java-2/src/main/java/algorithms/Evaluate.java
+++ java-2/src/main/java/algorithms/Evaluate.java
@@ -1,4 +1,4 @@
-package algoriyhms;
+package algorithms;
 
 import java.util.Scanner;
 import java.util.Stack;
@@ -11,29 +11,32 @@ public class Evaluate {
         int index=0,len=exc.length();
         while(index<len){
             Character s=exc.charAt(index++);
+            //System.out.println("|"+s);
             if(s.equals('(')){
                 //不操作
             } else if (s.equals('+')||s.equals('-')||s.equals('*')||s.equals('/')) {
                 ops.push(s);
-            } else if (s.equals(')')||index==len-1) {
+            } else if (s.equals(')')) {
                 //弹出运算符和操作数，计算结果并压入栈
                 Character op=ops.pop();
                 double v=val.pop();
                 if(op.equals('+')){
                     v+=val.pop();
                 } else if (op.equals('-')) {
-                    v-=val.pop();
+                    v=val.pop()-v;
                 } else if (op.equals('*')) {
                     v*=val.pop();
                 } else if (op.equals('/')) {
-                    v/=val.pop();
-                }
-            }else{
-                double v=s-'0';
-                while(++index<len&&exc.charAt(index)<='9'&&exc.charAt(index)>='0'){
-                    v=v*10+exc.charAt(index)-'0';
+                    v=val.pop()/v;
                 }
                 val.push(v);
+            }else{
+                /*double v=s-'0';
+
+                while(index+1<len&&exc.charAt(index+1)<='9'&&exc.charAt(index+1)>='0'){
+                    v=v*10+exc.charAt(++index)-'0';
+                }*/
+                val.push(Double.parseDouble(s.toString()));
             }
         }
         System.out.println(val.pop());
