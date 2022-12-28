6e437d7bb0dccd4bc9c93956cc46c5c7b3189c9f
123kangning <3390142252@qq.com>
2022-12-28 19:32:34 +0800

    1

--- java-1/src/main/java/test1/F001.java
+++ java-1/src/main/java/test1/F001.java
@@ -4,8 +4,16 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class F001 {
-    public static void main(String... args) throws IOException {
+import static project.Huffman.*;
 
+public class F001 {
+    public static void main(String... args){
+        String numCode="1010011011001001010100110011001101010010010100100101010100101101010";
+        int[] num=ChToBit(numCode,0);
+        String ans=BitToCh(num,numCode.length());
+        System.out.println(numCode);
+        System.out.println(ans);
+        System.out.println(numCode.equals(ans));
     }
+
 }
